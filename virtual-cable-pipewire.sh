c0e0d46dda6bfd6663c1de1ab2509ea4823fa4af
Steven Drinnan <72387419+steven3363@users.noreply.github.com>
2024-06-02 11:18:28 +0800

    Update virtual-cable-pipewire.sh
    Wrong code before hehehe
    This is the correct code for Pipewire

--- virtual-cable-pipewire.sh
+++ virtual-cable-pipewire.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-# Remove previously created Nodes
-#pw-cli dump short | egrep "sink|source|tunnel" | grep Node | cut -d: -f1 | xargs -L 1 -r pw-cli destroy 
+# Create a virtual sink that can be set as a monitor in OBS
+pactl load-module module-null-sink sink_name=VirtualSpeaker sink_properties=device.description=VirtualSpeaker
 
-# Create sink and source
-#pactl load-module module-null-sink sink_name=source object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
-#pactl load-module module-null-sink sink_name=sink object.linger=1 media.class=Audio/Sink channel_map=FL,FR
+# Link it with a virtual source that is visible in pulseaudio apps like Zoom
+pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=VirtualMic channel_map=front-left,front-right
+pw-link VirtualSpeaker:monitor_FL VirtualMic:input_FL
+pw-link VirtualSpeaker:monitor_FR VirtualMic:input_FR
 
-# Link sink with source
-#pw-link sink:monitor_1 source:input_1
-#pw-link sink:monitor_2 source:input_2
+#needs pulseaudio-utils 
 
 
 #from https://luke.hsiao.dev/blog/pipewire-virtual-microphone/
